ea3cc8800aba7bd3c2866770015382e8b0ea0f3e
natakoltunova <59485695+natakoltunova@users.noreply.github.com>
2021-03-21 20:00:40 +0300

    Add files via upload
    modal for sertificate section

--- js/script.js
+++ js/script.js
@@ -30,3 +30,23 @@ function showSlides(num) {
   slides[slideIndex - 1].style.display = 'flex'
   dots[slideIndex - 1].className += ' active'
 }
+
+// Get the modal
+let modal = document.getElementById('modal')
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+let img = document.getElementById('sertificate')
+let modalImg = document.getElementById('img01')
+
+img.onclick = function () {
+  modal.style.display = 'block'
+  modalImg.src = this.src
+}
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName('sertificate__close')[0]
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = 'none'
+}
