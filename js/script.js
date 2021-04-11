17bea2f1fd70004f7c6909730269e954b5cf5482
natakoltunova <59485695+natakoltunova@users.noreply.github.com>
2021-04-11 22:31:49 +0300

    add form

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 //select all links to the anchor on the page
 const linkNav = document.querySelectorAll('[href^="#"]')
 // speed can have a fractional value through a point (the lower the value - the higher the speed)
-const speed = 0.6
+const speed = 0.2
 
 for (let i = 0; i < linkNav.length; i++) {
   linkNav[i].addEventListener(
@@ -72,7 +72,7 @@ function showSlides(num) {
   dots[slideIndex - 1].className += ' active'
 }
 
-// РАЗДЕЛИТЬ
+// MODAL
 
 function getModal(id) {
   let modal = document.getElementById(id)
@@ -141,3 +141,86 @@ getImg('sertificate6').onclick = function () {
     getModal('modal6').style.display = 'none'
   }
 }
+
+//FORM
+document.addEventListener('DOMContentLoaded', function () {
+  //intercept (перехватить) form submission on button click
+  const form = document.getElementById('form') //assignment (присваивание) object "form" to variable
+  form.addEventListener('submit', formSend) //hanging event on variable (when submitting form go to function formSend)
+
+  async function formSend(e) {
+    e.preventDefault() //canceling standard behavior for form
+    //check validation
+    let error = formValidate(form)
+
+    let formData = new formData(form) //getting form data
+
+    if (error === 0) {
+      //if form is filled out correctly, inform user that the data is being sent
+      form.classList.add('_sending')
+      //sending data via ajax
+      //with POST send formData in sendmail.php
+      let response = await fetch('sendmail.php', {
+        method: 'POST',
+        body: formData,
+      })
+      //get answer if the data was sent successfully
+      if (response.ok) {
+        //sendmail.php will return json-response if everyth. ok
+        let result = await response.json()
+        alert(result.message)
+        form.reset
+        form.classList.remove('_sending')
+      } else {
+        alert('Ошибка!')
+        form.classList.remove('_sending')
+      }
+    } else {
+      alert('Заполните обязательные поля!')
+    }
+  }
+
+  function formValidate(form) {
+    let error = 0
+    let formReq = document.querySelectorAll('._req') //take all objects with class '._req' (required)
+
+    for (let index = 0; index < formReq.length; index++) {
+      const input = formReq[index]
+      formRemoveError(input) //remove the class '_error' before checking
+
+      if (input.classList.contains('_email')) {
+        if (emailTest(input)) {
+          formAddError(input)
+          error++
+        }
+      } else if (
+        input.getAttribute('type') === 'checkbox' &&
+        input.checked === false
+      ) {
+        formAddError(input)
+        error++
+      } else {
+        //checking if the field is filled at all
+        if (input.value === '') {
+          formAddError(input)
+          error++
+        }
+      }
+    }
+    return error
+  }
+
+  function formAddError(input) {
+    input.parentElement.classList.add('_error')
+    input.classList.add('_error')
+  }
+
+  function formRemoveError(input) {
+    input.parentElement.classList.remove('_error')
+    input.classList.remove('_error')
+  }
+
+  function emailTest(input) {
+    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value)
+  }
+})
